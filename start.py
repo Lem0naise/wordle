0a53e3a1270b7484aa0f55a7ddcd6dd458369550
Zac Nolan <zac1i1nolan@gmail.com>
2022-02-28 08:37:20 +0000

    Add files via upload

---
+++ start.py
@@ -0,0 +1,123 @@
+import game as wordle
+import os
+from time import sleep
+from PyDictionary import PyDictionary
+
+dictionary=PyDictionary() # dictionary
+
+
+def clear(): os.system('cls' if os.name == 'nt' else 'clear')
+
+print("Collecting wordle words...")
+
+words = wordle.Words()
+colours = wordle.colours()
+print(words.answer)
+clear()
+
+
+guess_number = 1
+
+guess = ""
+output = ["", ""] # placeholder for alphabet
+
+
+alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+clear()
+
+
+while guess != words.answer and guess_number<=6:
+
+    guess = input(f"Guess {guess_number}/6: ") # allow user to guess
+    guess = guess.strip().upper() # strip and upper it
+    
+
+    if guess.lower() in words.all_words or guess.lower() in words.answers:
+        # the word is valid but not right
+
+        guessed = words.guess(guess) # formatted guess display from wordle file
+        output.append(f"Guess {guess_number}/6: {guessed}") # adding the number and guessed word to the output 
+
+        if guess != words.answer:
+            guess_number+=1 # increment guess number if guess not correct
+
+        
+
+    else:
+        # the word is invalid
+
+        if len(guess)!=5:
+
+            print("Not right length.")
+            sleep(0.5)
+        else:
+                
+            print("Not a word")
+            sleep(0.5)
+
+
+    # colouring the alphabet at the top
+
+    for each in words.corr_letters:
+
+        if each.upper() in alphabet: alphabet[alphabet.index(each.upper().strip())] = (colours.RIGHT + each.upper() + colours.ENDC)
+
+    for each in words.incorr_letters:
+
+        if each.upper() in alphabet: alphabet[alphabet.index(each.upper().strip())] = (colours.WRONG + each.upper() + colours.ENDC)
+
+ 
+    clear() # clear screen
+
+    output[0] = ("".join([(i) for i in alphabet])) # adding alphabet to the output
+
+    for i in output: # print the previous guesses
+        print(i)
+
+
+clear()
+
+for i in output:
+    print(i)
+
+print()
+
+if guess_number<=6:
+
+    print("\x1B[38;5;10m") # greeny text
+    print(f"Yay! You got it in {guess_number}/6 guesses. ")
+    print(colours.ENDC) # end character
+
+else:
+
+    print("\x1B[38;5;196m") # greeny text
+    print(f"You didn't quite get it.")
+    print(colours.ENDC) # end character
+
+print()
+
+
+print('\033[1m'+ words.answer.lower() +'\033[0m') # printing word in bold
+
+
+# Printing word definition if possible
+try:
+    if dictionary.meaning(words.answer) != None:
+        for each in dictionary.meaning(words.answer): 
+
+            print('\033[4m'+ each +'\033[0m') # printing the type of word (verb / noun) in underlined
+            
+
+            for each in dictionary.meaning(words.answer)[each]:
+                
+                print('\033[3m'+each + '\033[0m') # printing the definitions of the word in italics
+
+            print()
+    else:
+        print("No definition available.")
+
+except:
+    print("No definition available.")
+
+print("\n\n\n\n\n\n\n\n\n")
